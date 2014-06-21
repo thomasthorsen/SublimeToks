6bdfa3b7c028ea1e6241bb199c4b50e5a489e0b9
Thomas Thorsen <ttdkgit@gmail.com>
2014-06-21 21:59:54 +0200

    Complete rewrite to solve some fundamental issues and add some new features including the ability to automatically index files on save to reduce the need for manually initiating the indexer. The index is now saved next to the project file instead of in the first project directory.

--- SublimeToks.py
+++ SublimeToks.py
@@ -1,12 +1,10 @@
 
-
 import sublime, sublime_plugin
 import os
 import re
 import subprocess
 import string
 import threading
-
 import time
 
 type_strings = {
@@ -48,66 +46,40 @@ def get_setting(key, default=None, view=None):
         pass
     return get_settings().get(key, default)
 
-def getEncodedPosition(file_name, row, col):
-    return file_name + ":" + str(row) + ":" + str(col)
-
-def getCurrentPosition(view):
-    row, col = view.rowcol(view.sel()[0].a)
-    return getEncodedPosition(view.file_name(), row + 1, col + 1)
-
-def isOnSameLine(pos1, pos2):
-    match1 = re.match("([^:]+):(\d+):(\d+)", pos1)
-    match2 = re.match("([^:]+):(\d+):(\d+)", pos2)
-    if match1.group(1) == match2.group(1) and \
-       match1.group(2) == match2.group(2) and \
-       match1.group(3) == match2.group(3):
-        return True
-    return False
-
-class GobackCommand(sublime_plugin.TextCommand):
-    def __init__(self, view):
-        self.view = view
-
-    def run(self, edit):
-        ToksCommand.navigate_back(self.view)
-
-class ForwardCommand(sublime_plugin.TextCommand):
-    def __init__(self, view):
-        self.view = view
-
-    def run(self, edit):
-        ToksCommand.navigate_forward(self.view)
-
 class SublimeToksIndexer(threading.Thread):
-    def __init__(self, directory):
+    def __init__(self, index, filenames=None):
         super(SublimeToksIndexer, self).__init__()
-        self.directory = directory
-        self.matches = None
+        self.index = index
+        self.filenames = filenames
 
     def run(self):
         targets = []
-        m = re.compile(".*\\.[ch]$")
+        m = re.compile(".*\\.(" + get_setting("filename_extensions") + ")$")
         for pdir in sublime.active_window().folders():
             for root, dirs, files in os.walk(pdir):
                 for file in files:
                     if m.match(file):
-                        targets.append(os.path.join(root, file))
+                        filename = os.path.join(root, file)
+                        if self.filenames == None or filename in self.filenames:
+                            targets.append(filename)
+        if len(targets) > 0:
+            cmd = ['toks', '-i', self.index, '-F', '-']
+            popen_arg_list = {
+                "stdin": subprocess.PIPE,
+            }
+            if (sublime.platform() == "windows"):
+                popen_arg_list["creationflags"] = 0x08000000
 
-        cmd = ["toks -F -"]
-        proc = subprocess.Popen(cmd, cwd=self.directory, shell=True,
-            stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        proc.communicate(bytes('\n'.join(targets), 'UTF-8'))
-        if proc.returncode != 0:
-            sublime.error_message("SublimeToks: Failed to invoke the toks indexer, please make sure you have it installed and added to the search path")
+            proc = subprocess.Popen(cmd, **popen_arg_list)
+            proc.communicate(bytes('\n'.join(targets), 'UTF-8'))
+
+            if proc.returncode != 0:
+                sublime.error_message("SublimeToks: Failed to invoke the toks indexer, please make sure you have it installed and added to the search path")
 
 class SublimeToksSearcher(threading.Thread):
-
-    def __init__(self, view, platform, root, database, symbol, mode):
+    def __init__(self, index, symbol, mode):
         super(SublimeToksSearcher, self).__init__()
-        self.view = view
-        self.platform = platform
-        self.root = root
-        self.database = database
+        self.index = index
         self.symbol = symbol
         self.mode = mode
 
@@ -125,142 +97,154 @@ class SublimeToksSearcher(threading.Thread):
         return output
 
     def run(self):
-        newline = ''
-        if self.platform == "windows":
-            newline = '\r\n'
-        else:
-            newline = '\n'
-
-        cscope_arg_list = ['toks', '-i', self.database, '--id', self.symbol]
+        cmd = ['toks', '-i', self.index, '--id', self.symbol]
         if self.mode != "any":
-            cscope_arg_list.append('--' + str(self.mode))
+            cmd.append('--' + str(self.mode))
         popen_arg_list = {
-            "shell": False,
             "stdout": subprocess.PIPE,
             "stderr": subprocess.PIPE
         }
-        if (self.platform == "windows"):
+        if (sublime.platform() == "windows"):
             popen_arg_list["creationflags"] = 0x08000000
 
-        proc = subprocess.Popen(cscope_arg_list, **popen_arg_list)
-        output = proc.communicate()[0].decode("utf-8").split(newline)
-        # print(output)
+        proc = subprocess.Popen(cmd, **popen_arg_list)
+        output = proc.communicate()[0].decode("utf-8").splitlines()
 
         self.matches = []
-        self.light_matches = []
         for line in output:
             match = self.match_output_line(line)
             if match != None:
                 self.matches.append(match)
-                self.light_matches.append(match[0] + " <" + match[1] + ">")
-                # print "File ", match.group(1), ", Line ", match.group(2), ", Instance ", match.group(3)
 
-class ToksCommand(sublime_plugin.TextCommand):
-    _backLines = []
-    _forwardLines = []
-    pre_lookup_position = None
+class ToksEventListener(sublime_plugin.EventListener):
+    def on_post_save(self, view):
+        view.window().run_command("toks", {"mode": "index_one", "filename": view.file_name()})
 
-    @staticmethod
-    def is_history_empty():
-        return len(ToksCommand._backLines) == 0
+class ToksCommand(sublime_plugin.WindowCommand):
+    def __init__(self, window):
+        super(ToksCommand, self).__init__(window)
+        self.worker = None
+        self.back_lines = []
+        self.forward_lines = []
+        self.index_one_files = []
 
-    @staticmethod
-    def is_future_empty():
-        return len(ToksCommand._forwardLines) == 0
+    def active_view_position(self):
+        row, col = self.view.rowcol(self.view.sel()[0].a)
+        return self.view.file_name() + ":" + str(row + 1) + ":" + str(col + 1)
 
-    @staticmethod
-    def navigate_back(view):
-        if not ToksCommand.is_history_empty():
-            current_position = getCurrentPosition(view)
-            if ToksCommand.is_future_empty() or not isOnSameLine(ToksCommand._forwardLines[0], current_position):
-                ToksCommand._forwardLines.insert(0, current_position)
-            while len(ToksCommand._backLines) > 1 and isOnSameLine(ToksCommand._backLines[0], current_position):
-                ToksCommand._backLines = ToksCommand._backLines[1:]
-            if not ToksCommand.is_history_empty():
-                encoded_position = ToksCommand._backLines[0]
+    def is_same_location(self, pos1, pos2):
+        match1 = re.match("([^:]+):(\d+):(\d+)", pos1)
+        match2 = re.match("([^:]+):(\d+):(\d+)", pos2)
+        if match1.group(1) == match2.group(1) and \
+           match1.group(2) == match2.group(2) and \
+           match1.group(3) == match2.group(3):
+            return True
+        return False
+
+    def is_history_empty(self):
+        return len(self.back_lines) == 0
+
+    def is_future_empty(self):
+        return len(self.forward_lines) == 0
+
+    def navigate_back(self):
+        if not self.is_history_empty():
+            current_position = self.active_view_position()
+            if self.is_future_empty() or not self.is_same_location(self.forward_lines[0], current_position):
+                self.forward_lines.insert(0, current_position)
+            while len(self.back_lines) > 1 and self.is_same_location(self.back_lines[0], current_position):
+                self.back_lines = self.back_lines[1:]
+            if not self.is_history_empty():
+                encoded_position = self.back_lines[0]
                 sublime.active_window().open_file(encoded_position, sublime.ENCODED_POSITION)
-                while len(ToksCommand._forwardLines) > 1 and isOnSameLine(ToksCommand._forwardLines[0], encoded_position):
-                    ToksCommand._forwardLines = ToksCommand._forwardLines[1:]
+                while len(self.forward_lines) > 1 and self.is_same_location(self.forward_lines[0], encoded_position):
+                    self.forward_lines = self.forward_lines[1:]
 
-    @staticmethod
-    def navigate_forward(view):
-        if not ToksCommand.is_future_empty():
-            encoded_position = ToksCommand._forwardLines[0]
-            ToksCommand._forwardLines = ToksCommand._forwardLines[1:]
-            current_position = getCurrentPosition(view)
-            if ToksCommand.is_history_empty() or not isOnSameLine(ToksCommand._backLines[0], current_position):
-                ToksCommand._backLines.insert(0, current_position)
-            if ToksCommand.is_history_empty() or not isOnSameLine(ToksCommand._backLines[0], encoded_position):
-                ToksCommand._backLines.insert(0, encoded_position)
+    def navigate_forward(self):
+        if not self.is_future_empty():
+            encoded_position = self.forward_lines[0]
+            self.forward_lines = self.forward_lines[1:]
+            current_position = self.active_view_position()
+            if self.is_history_empty() or not self.is_same_location(self.back_lines[0], current_position):
+                self.back_lines.insert(0, current_position)
+            if self.is_history_empty() or not self.is_same_location(self.back_lines[0], encoded_position):
+                self.back_lines.insert(0, encoded_position)
             sublime.active_window().open_file(encoded_position, sublime.ENCODED_POSITION)
 
-    @staticmethod
-    def navigate_jump(view, source, destination):
-        if ToksCommand.is_history_empty() or not isOnSameLine(ToksCommand._backLines[0], source):
-            ToksCommand._backLines.insert(0, source)
-        if ToksCommand.is_history_empty() or not isOnSameLine(ToksCommand._backLines[0], destination):
-            ToksCommand._backLines.insert(0, destination)
-        ToksCommand._forwardLines = []
+    def navigate_jump(self, source, destination):
+        if self.is_history_empty() or not self.is_same_location(self.back_lines[0], source):
+            self.back_lines.insert(0, source)
+        if self.is_history_empty() or not self.is_same_location(self.back_lines[0], destination):
+            self.back_lines.insert(0, destination)
+        self.forward_lines = []
         sublime.active_window().open_file(destination, sublime.ENCODED_POSITION)
-
-    def __init__(self, view):
-        self.view = view
-        self.database = None
-        settings = get_settings()
-
-    def look_for_database(self):
-        pdirs = sublime.active_window().folders()
-        for directory in pdirs:
-            if ("TOKS" in os.listdir(directory)):
-                return directory
-        if (len(pdirs) > 0):
-            return pdirs[0]
 
     def on_select(self, index):
         if index != -1:
-            destination = os.path.join(self.root, self.worker.matches[index][0])
-            self.navigate_jump(self.view, self.pre_lookup_position, destination)
+            self.navigate_jump(self.pre_lookup_position, self.worker.matches[index][0])
         else:
             sublime.active_window().open_file(self.pre_lookup_position, sublime.ENCODED_POSITION)
 
     def on_highlighted(self, index):
-        destination = os.path.join(self.root, self.worker.matches[index][0])
-        sublime.active_window().open_file(destination, sublime.ENCODED_POSITION | sublime.TRANSIENT)
+        sublime.active_window().open_file(self.worker.matches[index][0], sublime.ENCODED_POSITION | sublime.TRANSIENT)
 
-    def update_status(self, worker, message, complete, count=0, dir=1):
-        if self.worker == worker:
-            if worker.is_alive():
-                count = count + dir
-                if count == 7:
-                    dir = -1
-                elif count == 0:
-                    dir = 1
-                self.view.set_status("SublimeToks", message + " [%s=%s]" %
-                                    (' ' * count, ' ' * (7 - count)))
-                sublime.set_timeout(lambda: self.update_status(worker, message, complete, count, dir), 100)
-            else:
-                self.view.erase_status("SublimeToks")
+    def update_status(self, message, complete, count=0, dir=1):
+        if self.worker.is_alive():
+            count = count + dir
+            if count == 7:
+                dir = -1
+            elif count == 0:
+                dir = 1
+            self.view.set_status("SublimeToks", message + " [%s=%s]" % (' ' * count, ' ' * (7 - count)))
+            sublime.set_timeout(lambda: self.update_status(message, complete, count, dir), 100)
+        else:
+            self.view.erase_status("SublimeToks")
+            if complete:
                 complete()
 
-    def run(self, edit, mode):
+    def deferred_index_one(self):
+        if self.worker and self.worker.is_alive():
+            sublime.set_timeout(lambda: self.deferred_index_one(), 500)
+        else:
+            self.worker = SublimeToksIndexer(self.index, self.index_one_files)
+            self.index_one_files = []
+            self.worker.start()
+
+    def run(self, mode, filename=None):
+        project_file_name = self.window.project_file_name()
+        if not project_file_name and mode != "index_one":
+            sublime.error_message("SublimeToks: Please create a project to enable indexing")
+            return
+
+        self.index = project_file_name.replace(".sublime-project", ".sublime-toks")
+        if self.index == project_file_name:
+            self.index = project_file_name + ".sublime-toks"
+        self.view = self.window.active_view()
+
+        if mode == "back":
+            self.navigate_back()
+            return
+
+        if mode == "forward":
+            self.navigate_forward()
+            return
+
+        if mode == "index_one":
+            self.index_one_files.append(filename)
+            if len(self.index_one_files) == 1:
+                sublime.set_timeout(lambda: self.deferred_index_one(), 500)
+            return
+
+        if self.worker and self.worker.is_alive():
+            return
+
         self.mode = mode
 
-        # Cancel any running worker
-        self.worker = None
-
-        # Look for database
-        if self.database == None:
-            self.root = self.look_for_database()
-            if self.root == None:
-                sublime.error_message("SublimeToks: Please add at least one directory to the project to enable indexing")
-                return
-            self.database = os.path.join(self.root, "TOKS")
-
-        # Check if database exists
-        if not "TOKS" in os.listdir(self.root) or self.mode == "index":
-            self.worker = SublimeToksIndexer(self.root)
+        # Check if index exists
+        if mode == "index" or not os.path.isfile(self.index):
+            self.worker = SublimeToksIndexer(self.index)
             self.worker.start()
-            self.update_status(self.worker, "Indexing", self.on_indexing_complete)
+            self.update_status("Indexing", self.on_indexing_complete)
             return
 
         self.on_indexing_complete()
@@ -271,11 +255,11 @@ class ToksCommand(sublime_plugin.TextCommand):
             return
 
         # Save the pre lookup position
-        self.pre_lookup_position = getCurrentPosition(self.view)
+        self.pre_lookup_position = self.active_view_position()
 
         # Search for the first word that is selected.
         first_selection = self.view.word(self.view.sel()[0])
-        self.symbol = self.view.substr(first_selection)
+        symbol = self.view.substr(first_selection)
 
         # Show only the one selection picked for search
         self.view.sel().clear()
@@ -283,25 +267,20 @@ class ToksCommand(sublime_plugin.TextCommand):
 
         if get_setting("prompt_before_searching") == True:
             sublime.active_window().show_input_panel('Toks Symbol To Search:',
-                                                     self.symbol,
+                                                     symbol,
                                                      self.on_search_confirmed,
                                                      None,
                                                      None)
         else:
-            self.on_search_confirmed(self.symbol)
+            self.on_search_confirmed(symbol)
 
     def on_search_confirmed(self, symbol):
-        self.symbol = symbol
         self.worker = SublimeToksSearcher(
-                view = self.view,
-                platform = sublime.platform(),
-                root = self.root,
-                database = self.database,
+                index = self.index,
                 symbol = symbol,
                 mode = self.mode)
         self.worker.start()
-        self.update_status(self.worker, "Searching", self.on_lookup_complete)
+        self.update_status("Searching", self.on_lookup_complete)
 
     def on_lookup_complete(self):
-        #self.view.show_popup_menu(self.worker.light_matches, self.on_select)
         sublime.active_window().show_quick_panel(self.worker.matches, self.on_select, 0, 0, self.on_highlighted)
